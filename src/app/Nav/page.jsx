8664ef774b10f09c7ac8457f119574e2ef92b211
Mohit76655 <mohit76655@gmail.com>
2024-01-18 23:27:16 +0530

    Second Commit

---
+++ src/app/Nav/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 text-2xl">
+      Page
+    </div>
+  );
+};
+
+export default page;
