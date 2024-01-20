92f285e99055d394e0bfe10b0e5ff87d12d7f19f
Mohit76655 <mohit76655@gmail.com>
2024-01-20 19:03:56 +0530

    Social-links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,19 +2,33 @@ import About from "@/components/About";
 import Image from "next/image";
 import Link from "next/link";
 import Portfoilio from "./portfolio/page";
+import "remixicon/fonts/remixicon.css";
 
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center text-2xl">
-        <div className=" mb-14 flex flex-col gap-16">
-          <p className="ml-28 font-semibold text-[#323232]">I AM</p>
-          <h1 className="font-bold text-[9rem]">MOHIT</h1>
-          <div className="">
-            <p className=" ml-64 text-[#323232]">A Frontend</p>
-            <p className=" ml-80 text-[#323232]">Developer</p>
+      <main className="min-h-screen pt-[12rem]">
+        <div className="flex justify-center items-center text-2xl">
+          <div className=" mb-20 flex flex-col md:gap-16 gap-10">
+            <p className="md:ml-28 ml-12 font-semibold text-[#323232]">I AM</p>
+            <h1 className="font-bold md:text-[9rem] text-[4rem]">MOHIT</h1>
+            <div className="">
+              <p className=" md:ml-64 ml-16 text-[#323232]">A Frontend</p>
+              <p className=" md:ml-80 ml-24 text-[#323232]">Developer</p>
+            </div>
           </div>
         </div>
+        <span className=" flex flex-col pl-5 text-xl">
+          <a href="https://github.com/Mohit76655">
+            <i className="ri-github-line"></i>
+          </a>
+          <a href="https://twitter.com/_MoHiT_K">
+            <i className="ri-twitter-fill"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/mohit-kachhwaha-9577a3274/">
+            <i className="ri-linkedin-fill"></i>
+          </a>
+        </span>
       </main>
       <About />
       <Portfoilio />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,18 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className=" sticky bg-transparent text-[#DEE0DD] flex justify-between p-4 px-8 mt-7">
+    <header className=" sticky bg-transparent text-[#DEE0DD] flex justify-between p-4 px-8 mt-4">
       <div className=" font-bold text-lg">
         <Link href="/">Mohit</Link>
       </div>
-      <div>
+      <div className=" hidden md:block">
         <ul className=" flex gap-12 font-semibold">
           <Link href="/portfolio">PROJECTS</Link>
           <Link href="/contact">CONTACT</Link>
         </ul>
+      </div>
+      <div className="md:hidden right-0">
+        <i className="ri-menu-line text-2xl"></i>
       </div>
     </header>
   );
