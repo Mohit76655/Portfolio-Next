1dedc08ff5ac301032bffe1de43f97320d21d545
Mohit76655 <mohit76655@gmail.com>
2024-01-21 01:55:23 +0530

    BUG-Fix

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact page</div>;
+  return <div className=" min-h-screen">Contact page</div>;
 };
 
 export default Contact;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Portfoilio = () => {
   return (
-    <div className=" ">
+    <div className=" min-h-screen">
       <div className="">PROJECT</div>
     </div>
   );
